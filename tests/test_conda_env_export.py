d0a478d9d33143b72fc564e9932eac2037a04eda
Mirochill <200482516+Mirochill@users.noreply.github.com>
2026-05-23 14:42:53 +0200

    Avoid pkg_resources for pip package export

--- tests/test_conda_env_export.py
+++ tests/test_conda_env_export.py
@@ -29,3 +29,36 @@ class TestConda_env_export(unittest.TestCase):
         help_result = runner.invoke(cli.main, ['--help'])
         assert help_result.exit_code == 0
         assert 'Show this message and exit.' in help_result.output
+
+    def test_get_pip_deps_uses_iterated_packages(self):
+        """Test pip export through the package iterator adapter."""
+        class FakeRequirement(object):
+            key = 'dependency'
+            specs = [('>=', '2')]
+
+        class FakePackage(object):
+            def __init__(self, key, version, project_name, requirements=()):
+                self.key = key
+                self.version = version
+                self.project_name = project_name
+                self._requirements = requirements
+
+            def requires(self):
+                return self._requirements
+
+        packages = [
+            FakePackage('root', '1.0', 'Root', [FakeRequirement()]),
+            FakePackage('dependency', '2.0', 'Dependency'),
+        ]
+        original = conda_env_export._iter_pip_packages
+        conda_env_export._iter_pip_packages = lambda paths: packages
+        try:
+            nodes = conda_env_export.CondaEnvExport().get_pip_deps(
+                ['/env/site-packages'], all=True)
+        finally:
+            conda_env_export._iter_pip_packages = original
+
+        assert [str(node) for node in nodes] == [
+            'Dependency==2.0',
+            'Root==1.0',
+        ]
